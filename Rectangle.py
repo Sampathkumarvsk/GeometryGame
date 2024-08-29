4fc8c02c04dc1aa4d99f2907e6eb88295887d37d
Sampath <sampathkumar.vemulakonda@gmail.com>
2024-08-29 22:13:05 +0530

    Add Rectangle and Point classes

---
+++ Rectangle.py
@@ -0,0 +1,7 @@
+from Point import Point
+class Rectangle:
+    def __init__(self,lowleft,upright):
+        self.lowleft = lowleft
+        self.upright = upright
+pointx = Point(6,7)
+rectanglex = Rectangle(Point(5,6), Point(7,9))
